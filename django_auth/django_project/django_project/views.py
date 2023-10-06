0025effd73a8bc69fd95d70f9535836ce004f587
Peyton Kelly <pkelly5511@gmail.com>
2023-10-06 13:54:30 -0400

    Meta data function

--- django_auth/django_project/django_project/views.py
+++ django_auth/django_project/django_project/views.py
@@ -1,9 +1,10 @@
 # pip install django-ipware
 import ipaddress
 
-import requests
+
 
 from ip2geotools.databases.noncommercial import DbIpCity
+
 
 
 def get_client_ip_address(request):
@@ -17,13 +18,21 @@ def get_client_ip_address(request):
 # returns client IP address as a string
 #puts ip address into a variable for next function
 
+# add the returned ip_addr to db.sqlite3
+# add the returned ip_addr to db.sqlite3
+def add_ip_to_db(ip_addr, request):
+    ip_addr = get_client_ip_address(request)
+    ip_addr = ipaddress.ip_address(ip_addr)
+    ip_addr.save()
+    
 
 
-def get_client_location(ip):
-    ip = get_client_ip_address(requests)
+def get_client_location(ip, request):
+    ip = get_client_ip_address(request)
     res = DbIpCity.get(ip, api_key="free")
     print(f"IP Address: {res.ip_address}")
     print(f"Location: {res.city}, {res.region}, {res.country}")
     print(f"Coordinates: (Lat: {res.latitude}, Lng: {res.longitude})")
       # 198.35.26.96
-    get_client_location(ip)
+    print(get_client_location(ip))
+

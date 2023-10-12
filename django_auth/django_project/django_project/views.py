71e7efda2612fc4eaef471304eed367c18c7c3ce
Peyton Kelly <pkelly5511@gmail.com>
2023-10-12 11:02:29 -0400

    Views IP

--- django_auth/django_project/django_project/views.py
+++ django_auth/django_project/django_project/views.py
@@ -1,38 +1,25 @@
 # pip install django-ipware
 import ipaddress
-
-
-
 from ip2geotools.databases.noncommercial import DbIpCity
 
-
-
-def get_client_ip_address(request):
-    req_headers = request.META
-    x_forwarded_for_value = req_headers.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for_value:
-        ip_addr = x_forwarded_for_value.split(',')[-1].strip()
+def Index(request):
+    context = {}
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for is not None:
+        ip = x_forwarded_for.split(',')[0]
     else:
-        ip_addr = req_headers.get('REMOTE_ADDR')
-    return ip_addr
+        ip = request.META.get('REMOTE_ADDR')
+    print("IP OF USER: ",ip)
+
+
 # returns client IP address as a string
 #puts ip address into a variable for next function
 
 # add the returned ip_addr to db.sqlite3
 # add the returned ip_addr to db.sqlite3
-def add_ip_to_db(ip_addr, request):
-    ip_addr = get_client_ip_address(request)
-    ip_addr = ipaddress.ip_address(ip_addr)
-    ip_addr.save()
+
     
 
 
-def get_client_location(ip, request):
-    ip = get_client_ip_address(request)
-    res = DbIpCity.get(ip, api_key="free")
-    print(f"IP Address: {res.ip_address}")
-    print(f"Location: {res.city}, {res.region}, {res.country}")
-    print(f"Coordinates: (Lat: {res.latitude}, Lng: {res.longitude})")
-      # 198.35.26.96
-    print(get_client_location(ip))
 
+# compare IP info to database ip info

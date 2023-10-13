2346aca3bbced8a3acb2552773b4ccfe389ab2fd
Peyton Kelly <pkelly5511@gmail.com>
2023-10-12 22:54:56 -0400

    Redone after false start

---
+++ Code /UserIpProject/MyApp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import HttpResponse
+
+# Create Viewe
+
+def ipaddress(request):
+    user_ip = request.META.get('HTTP_X_FORWARDED_FOR')
+    if user_ip is not None:
+        ip = user_ip.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    
+    return HttpResponse("Welcome User!<br>You are visiting from: {}".format(ip))
+# Create your views here.
+
+# use the ip2location database to get the location of the user
+

8666a8544e3ddc88d35d24f5d076c5ad5ac032dd
Peyton Kelly <pkelly5511@gmail.com>
2024-01-31 14:42:19 -0500

    ipstack  api for IP requests on home page

--- Admin/website/urls.py
+++ Admin/website/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("", views.on_home, name="on_home"),
-
+    
     # path("login/", views.login_user, name="login"),
     path("logout/", views.logout_user, name="logout"),
     path("register/", views.register_user, name="register"),
--- Admin/website/views.py
+++ Admin/website/views.py
@@ -20,27 +20,53 @@ from django.core.paginator import Paginator
 
 
 def home(request):
+    # get the IP address of the user
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    response = requests.get('http://api.ipstack.com/'+ip+'?access_key=7ff9f8d117530b7a7a4ffaee163f1d55') #change from HTTP to HTTPS on the IPSTACK API if you have a premium account
+    rawData = response.json()
+    print(rawData) # print this out to look at the response
+    continent = rawData['continent_name']
+    country = rawData['country_name']
+    capital = rawData['city']
+    city = rawData['location']['capital']
+    now = datetime.now()
+    datetimenow = now.strftime("%Y-%m-%d")
+    saveNow = Monitor(
+        continent=continent,
+        country=country,
+        capital=capital,
+        city=city,
+        datetime=datetimenow,
+        ip=ip
+    )
+    saveNow.save()
+    
 
     records = Record.objects.all()
-
+    
     # check if logging in
     if request.method == "POST":
         # attempt to sign user in
         username = request.POST["username"]
         password = request.POST["password"]
         # authenticate user
-
+        
         user = authenticate(request, username=username, password=password)
         # check if authentication successful
         if user is not None:
             login(request, user)
+            
             messages.success(request, f"You have logged in, {username}!")
             return redirect("home")
         else:
             messages.error(request, "Invalid username or password.try again.")
             return redirect("home")
     else:
-
+        
         return render(request, "home.html", {"records": records})
 
 
@@ -168,56 +194,37 @@ def upload(request):
         return redirect("home")
 
 def traffic_monitor(request):
-    dataSaved = Monitor.objects.all().order_by('-datetime')
-    # Getting loadover15 minutes 
-    load1, load5, load15 = psutil.getloadavg()
-    cpu_usage = int((load15/os.cpu_count()) * 100)
-    ram_usage = int(psutil.virtual_memory()[2])
-    p = Paginator(dataSaved, 100)
-    #shows number of items in page
-    totalSiteVisits = (p.count)
-    #find unique page viewers & Duration
-    pageNum = request.GET.get('page', 1)
-    page1 = p.page(pageNum)
-    #unique page viewers
-    a = Monitor.objects.order_by().values('ip').distinct()
-    pp = Paginator(a, 10)
-    #shows number of items in page
-    unique = (pp.count)
-    #update time
-    now = datetime.now()
-    data = {
-        "now":now,
-        "unique":unique,
-        "totalSiteVisits":totalSiteVisits,
-        "cpu_usage": cpu_usage,
-        "ram_usage": ram_usage,
-        "dataSaved": page1,
-    }
-    return render(request, 'monitor.html', data)
-#home page
-def on_home(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
+    if request.user.is_authenticated:
+    # Getting loadover15 minutes
+        dataSaved = Monitor.objects.all().order_by('-datetime')
+        # Getting loadover15 minutes 
+        load1, load5, load15 = psutil.getloadavg()
+        cpu_usage = int((load15/os.cpu_count()) * 100)
+        ram_usage = int(psutil.virtual_memory()[2])
+        p = Paginator(dataSaved, 100)
+        #shows number of items in page
+        totalSiteVisits = (p.count)
+        #find unique page viewers & Duration
+        pageNum = request.GET.get('page', 1)
+        page1 = p.page(pageNum)
+        #unique page viewers
+        a = Monitor.objects.order_by().values('ip').distinct()
+        pp = Paginator(a, 10)
+        #shows number of items in page
+        unique = (pp.count)
+        #update time
+        now = datetime.now()
+        data = {
+            "now":now,
+            "unique":unique,
+            "totalSiteVisits":totalSiteVisits,
+            "cpu_usage": cpu_usage,
+            "ram_usage": ram_usage,
+            "dataSaved": page1,
+        }
+        return render(request, 'monitor.html', data)
     else:
-        ip = request.META.get('REMOTE_ADDR')
-    response = requests.get('http://api.ipstack.com/'+ip+'?access_key=GETYOURACCESSKEY') #change from HTTP to HTTPS on the IPSTACK API if you have a premium account
-    rawData = response.json()
-    print(rawData) # print this out to look at the response
-    continent = rawData['continent_name']
-    country = rawData['country_name']
-    capital = rawData['city']
-    city = rawData['location']['capital']
-    now = datetime.now()
-    datetimenow = now.strftime("%Y-%m-%d %H:%M:%S")
-    saveNow = Monitor(
-        continent=continent,
-        country=country,
-        capital=capital,
-        city=city,
-        datetime=datetimenow,
-        ip=ip
-    )
-    saveNow.save()
-    return render(request, 'home.html')
+        messages.success(request, "Please login to view this page.")
+        return redirect("home")
+
+
